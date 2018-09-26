c3306a6bb1835984b24487160b735bd701f83e6e
Oliver Salzburg <oliver.salzburg@gmail.com>
2018-09-26 17:18:42 +0200

    [FIX] Hook is missing shebang

--- hooks/iosBeforePrepareHook.js
+++ hooks/iosBeforePrepareHook.js
@@ -1,47 +1,52 @@
+#!/usr/bin/env node
+
 /*
 Hook executed before the 'prepare' stage. Only for iOS project.
 It will check if project name has changed. If so - it will change the name of the .entitlements file to remove that file duplicates.
 If file name has no changed - hook will do nothing.
 */
 
-var path = require('path');
-var fs = require('fs');
-var ConfigXmlHelper = require('./lib/configXmlHelper.js');
+const path            = require( "path" );
+const fs              = require( "fs" );
+const ConfigXmlHelper = require( "./lib/configXmlHelper.js" );
 
-module.exports = function(ctx) {
-  run(ctx);
+module.exports = context => {
+	run( context );
 };
 
 /**
  * Run the hook logic.
  *
- * @param {Object} ctx - cordova context object
+ * @param {Object} context - cordova context object
  */
-function run(ctx) {
-  var projectRoot = ctx.opts.projectRoot;
-  var iosProjectFilePath = path.join(projectRoot, 'platforms', 'ios');
-  var configXmlHelper = new ConfigXmlHelper(ctx);
-  var newProjectName = configXmlHelper.getProjectName();
+function run( context ) {
+	const projectRoot        = context.opts.projectRoot;
+	const iosProjectFilePath = path.join( projectRoot, "platforms", "ios" );
+	const configXmlHelper    = new ConfigXmlHelper( context );
+	const newProjectName     = configXmlHelper.getProjectName();
 
-  var oldProjectName = getOldProjectName(iosProjectFilePath);
+	const oldProjectName = getOldProjectName( iosProjectFilePath );
 
-  // if name has not changed - do nothing
-  if (oldProjectName.length && oldProjectName === newProjectName) {
-    return;
-  }
+	// if name has not changed - do nothing
+	if( oldProjectName.length && oldProjectName === newProjectName ) {
+		return;
+	}
 
-  console.log('Project name has changed. Renaming .entitlements file.');
+	// eslint-disable-next-line no-console
+	console.log( "Project name has changed. Renaming .entitlements file." );
 
-  // if it does - rename it
-  var oldEntitlementsFilePath = path.join(iosProjectFilePath, oldProjectName, 'Resources', oldProjectName + '.entitlements');
-  var newEntitlementsFilePath = path.join(iosProjectFilePath, oldProjectName, 'Resources', newProjectName + '.entitlements');
+	// if it does - rename it
+	const oldEntitlementsFilePath = path.join( iosProjectFilePath, oldProjectName, "Resources", `${oldProjectName}.entitlements` );
+	const newEntitlementsFilePath = path.join( iosProjectFilePath, oldProjectName, "Resources", `${newProjectName}.entitlements` );
 
-  try {
-    fs.renameSync(oldEntitlementsFilePath, newEntitlementsFilePath);
-  } catch (err) {
-    console.warn('Failed to rename .entitlements file.');
-    console.warn(err);
-  }
+	try {
+		fs.renameSync( oldEntitlementsFilePath, newEntitlementsFilePath );
+	} catch( err ) {
+		/* eslint-disable no-console */
+		console.warn( "Failed to rename .entitlements file." );
+		console.warn( err );
+	/* eslint-enable no-console */
+	}
 }
 
 // region Private API
@@ -53,22 +58,22 @@ function run(ctx) {
  * @param {String} projectDir absolute path to ios project directory
  * @return {String} old project name
  */
-function getOldProjectName(projectDir) {
-  var files = [];
-  try {
-    files = fs.readdirSync(projectDir);
-  } catch (err) {
-    return '';
-  }
+function getOldProjectName( projectDir ) {
+	let files = [];
+	try {
+		files = fs.readdirSync( projectDir );
+	} catch( err ) {
+		return "";
+	}
 
-  var projectFile = '';
-  files.forEach(function(fileName) {
-    if (path.extname(fileName) === '.xcodeproj') {
-      projectFile = path.basename(fileName, '.xcodeproj');
-    }
-  });
+	let projectFile = "";
+	files.forEach( fileName => {
+		if( path.extname( fileName ) === ".xcodeproj" ) {
+			projectFile = path.basename( fileName, ".xcodeproj" );
+		}
+	} );
 
-  return projectFile;
+	return projectFile;
 }
 
 // endregion
